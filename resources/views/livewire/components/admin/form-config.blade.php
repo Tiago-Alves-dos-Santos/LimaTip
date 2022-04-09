c1533cefa6ee908070a049ff1b5f91214c31cc1d
Tiago <tiagooliveiraaso2@gmail.com>
2022-04-08 22:17:08 -0300

    link storage public configurado

--- resources/views/livewire/components/admin/form-config.blade.php
+++ resources/views/livewire/components/admin/form-config.blade.php
@@ -2,16 +2,16 @@
     {{-- Because she competes with no one, no one can compete with her. --}}
     <form action="">
         <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-4 align-self-end">
                 <label for="">Limite de registros</label>
                 <input type="number" step="1" class="form-control" min="0">
             </div>
-            <div class="col-md-4">
+            <div class="col-md-4 align-self-end">
                 <label for="">Valor do pix</label>
                 <input type="text" class="form-control">
             </div>
             <div class="col-md-4">
-                <label for="formFile" class="form-label"></label>
+                <label for="formFile" class="form-label">Arquivo Atual</label>
                 <input class="form-control" type="file" id="formFile">
             </div>
         </div>

ebae1d4f04c1e8a4abfacf4fbebdf36c36c21cc2
Tiago <tiagooliveiraaso2@gmail.com>
2022-04-09 00:29:09 -0300

    campo id removido da tabela

--- resources/views/livewire/components/admin/table-registros.blade.php
+++ resources/views/livewire/components/admin/table-registros.blade.php
@@ -4,7 +4,6 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th scope="col">#</th>
                     <th scope="col">Nome</th>
                     <th scope="col">Telefone</th>
                     <th scope="col">Data</th>
@@ -12,19 +11,17 @@
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
                 </tr>
                 <tr>
-                    <th scope="row">2</th>
                     <td>Jacob</td>
                     <td>Thornton</td>
                     <td>@fat</td>
                 </tr>
                 <tr>
-                    <th scope="row">3</th>
+                    <td>ZuckerBerg</td>
                     <td colspan="2">Larry the Bird</td>
                     <td>@twitter</td>
                 </tr>
